206e422269e2d6d5b2b49d2e283028f90f8e52ea
tiagoo <thurstaltamirano@escuelasproa.edu.ar>
2024-03-19 09:01:51 -0300

    ponemos ejs de login

--- controllers/myController.js
+++ controllers/myController.js
@@ -6,4 +6,6 @@ exports.inicio = (req, res) => {
     res.status(200).render('index');
 };
 
-    
+exports.login = (req, res) => {
+    res.status(200).render('login');
+};
--- routes/myRouter.js
+++ routes/myRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 //Defino rutas y acciones de respuesta
 router.route('/').get(myController.inicio);
+router.route('/login').get(myController.login);
 
 
 module.exports = router;
