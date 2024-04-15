8eb1d84da9cf717b77eb6c8a00ef11003e528f40
tiagoo <thurstaltamirano@escuelasproa.edu.ar>
2024-04-15 11:08:06 -0300

    HASTA ACA ANDA

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -5,6 +5,6 @@ const router = express.Router();
 // Defino rutas y acciones de respuesta
 router.route('/').get(myController.inicio);
 router.route('/login').get(myController.login);
-router.route('/register').get(myController.registerPage).post(myController.register); // Agrego la ruta POST para el registro
-
+router.route('/register').post(myController.register); // Agrego la ruta POST para el registro
+router.route('/registerPage').get(myController.registerPage)
 module.exports = router;
